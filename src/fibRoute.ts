e247132c9f257f4462d17e25e4a0f2a8ab1cb55c
Fakpabio <foreverakp12@gmail.com>
2023-08-30 14:52:36 -0400

    fixing paramaters for fibRoute

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -2,8 +2,8 @@
 
 import fibonacci from "./fib";
 
-export default (req: { params: { num: any; }; }, res: { send: (arg0: string) => void; }) => {
-  const { num } = req.params as any;
+export default (req: { params: { num: string; }; }, res: { send: (arg0: string) => void; }) => {
+  const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
